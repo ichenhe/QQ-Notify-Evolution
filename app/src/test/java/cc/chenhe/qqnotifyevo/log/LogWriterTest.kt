5ceced1825435ecc7409bff5f05a86086a587f04
Chenhe <liangchenhe55@gmail.com>
2020-07-25 10:42:31 +0800

    修复 UnitTest IO Error (#6)

--- app/src/test/java/cc/chenhe/qqnotifyevo/log/LogWriterTest.kt
+++ app/src/test/java/cc/chenhe/qqnotifyevo/log/LogWriterTest.kt
@@ -4,6 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldNotBeEqualTo
 import org.junit.After
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import java.io.File
 import java.util.*
@@ -13,22 +14,33 @@ class LogWriterTest {
 
     companion object {
         private const val TIME = 1595582295000
+
+        private lateinit var cacheDir: File
+
+        @BeforeClass
+        @JvmStatic
+        fun classSetup() {
+            cacheDir = if (System.getenv("GITHUB_ACTIONS") == "true") {
+                File(System.getenv("HOME"), "qqevo_log_test")
+            } else {
+                File(System.getProperty("java.io.tmpdir"), "qqevo_log_test")
+            }
+            println("[LogWriterTest] Cache dir: ${cacheDir.absolutePath}")
+        }
     }
 
-    private lateinit var cacheDir: File
     private lateinit var writer: LogWriter
 
-    private fun createLogWriter(time: Long = System.currentTimeMillis()): LogWriter {
-        cacheDir = File(System.getProperty("java.io.tmpdir"), "qqevo_log_test")
-        return LogWriter(cacheDir, time)
+    private fun createLogWriter(): LogWriter {
+        return LogWriter(cacheDir, TIME)
     }
 
     @Before
     fun setup() {
-        writer = createLogWriter()
         if (cacheDir.isDirectory) {
             cacheDir.deleteRecursively()
         }
+        writer = createLogWriter()
     }
 
     @After
@@ -39,17 +51,17 @@ class LogWriterTest {
 
     @Test
     fun writeLog() {
-        writer.write("Test")
-        writer.write("Hello")
+        writer.write("Test", TIME)
+        writer.write("Hello", TIME)
         writer.logFile.readText() shouldBeEqualTo "Test\nHello\n"
     }
 
     @Test
     fun appendLog() {
-        createLogWriter(TIME).use { w ->
+        createLogWriter().use { w ->
             w.write("line1", TIME)
         }
-        createLogWriter(TIME).use { w ->
+        createLogWriter().use { w ->
             w.write("line2", TIME)
             w.logFile.readText() shouldBeEqualTo "line1\nline2\n"
         }
@@ -57,7 +69,7 @@ class LogWriterTest {
 
     @Test
     fun writeLog_differentDay() {
-        val calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance().apply { timeInMillis = TIME }
         writer.write("Test", calendar.timeInMillis)
         val f1 = writer.logFile
 
