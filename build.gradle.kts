91edb2d87596d80cc836764af05b11a9aa23347c
Chenhe <git@chenhe.me>
2023-11-04 00:30:10 +0000

    upgrade kotlin to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
 allprojects {
